15357a32e0be7a0824b7d631c2b2db02107d5bef
Leela Venkatesh Katabathula <klvenky@user.noreply.github.com>
2018-10-02 09:27:26 +0530

    solved 4

---
+++ solutions/004_largest_palindrome.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+from is_palindrome import is_palindrome
+min = 100
+pal = 0
+max = 1000
+for i in range(min, max):
+    j = i
+    while j < max:
+        prod = i * j
+        # print(i, ' * ', j, ' = ', prod)
+        pal = prod if (is_palindrome(prod) and prod > pal) else pal
+        j += 1
+print('max palindrome is {0}'.format(pal))
---
+++ solutions/is_palindrome.py
@@ -0,0 +1,3 @@
+def is_palindrome(val):
+  string_val = str(val)
+  return string_val == string_val[::-1]
